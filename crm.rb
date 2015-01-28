ee4c675ddf919f795ca10a8884adb1e101a827cf
Zachary Toth <zachtoth@gmail.com>
2015-01-27 20:25:55 -0500

    finished section 5

--- crm.rb
+++ crm.rb
@@ -1,16 +1,16 @@
 require 'sinatra'
 require_relative 'contact'
+require_relative 'rolodex'
 
+$rolodex= Rolodex.new
+
+#routes
 get '/' do 
 	@crm_app_name = "My CRM"
 	erb :index
 end
 
 get '/contacts' do
-	 @contacts = []
-  @contacts << Contact.new("Yehuda", "Katz", "yehuda@example.com", "Developer")
-  @contacts << Contact.new("Mark", "Zuckerberg", "mark@facebook.com", "CEO")
-  @contacts << Contact.new("Sergey", "Brin", "sergey@google.com", "Co-Founder")
 	erb :contacts
 end
 
